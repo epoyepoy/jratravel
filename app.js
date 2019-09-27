912a69b1d2285e78d07d999ea87be5c15fdb8f1b
Eugene Paul Cabauatan <eugenepaul83@gmail.com>
2019-09-27 22:08:54 +0400

    modified app.js

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const   express = require("express"),
-        app     = express();
+const express = require("express"),
+  app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.render("index"); 
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-    res.render("about"); 
+  res.render("about");
 });
 
 app.get("/blogs/start-your-own-travel-business", (req, res) => {
-    res.render("blogs/start-your-own-travel-business"); 
+  res.render("blogs/start-your-own-travel-business");
 });
 
 app.get("/blogs/top-places-to-visit-in-the-philippines", (req, res) => {
-    res.render("blogs/top-places-to-visit-in-the-philippines"); 
+  res.render("blogs/top-places-to-visit-in-the-philippines");
 });
 
 app.get("/blogs/budget-and-mid-range-boracay-hotels", (req, res) => {
-    res.render("blogs/budget-and-mid-range-boracay-hotels"); 
+  res.render("blogs/budget-and-mid-range-boracay-hotels");
 });
 
 app.use((req, res, next) => {
-    res.status(404).render("404");
+  res.status(404).render("404");
 });
 
-// app.listen(process.env.PORT, process.env.IP,() => {
-//    console.log("The Server Has Started!"); 
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log("The Server Has Started!");
+});
+
+// app.listen(3000,() => {
+//    console.log("The Server Has Started!");
 // });
-
-app.listen(3000,() => {
-   console.log("The Server Has Started!"); 
-});
